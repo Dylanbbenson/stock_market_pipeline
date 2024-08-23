d767ec7c4ff1a073df73b226b925aa2655fa3428
dylanbbenson <dylanbenson@duck.com>
2024-08-23 10:06:56 -0500

    Commit ingestion and scrape python scripts

---
+++ scripts/ingest_stock_market_data.py
@@ -0,0 +1,75 @@
+import requests
+import json
+import csv
+from datetime import date, datetime
+import os
+import pandas as pd
+
+current_date = date.today().strftime('%Y-%m-%d')
+current_time = datetime.now().strftime("%H")
+if not os.path.exists("../data"): os.makedirs("../data")
+json_file = f"../data/stock_market_data_{current_date}_{current_time}.json"
+csv_file = f"../data/stock_market_{current_date}_{current_time}.csv"
+ticker_file = f"../data/tickers_{current_date}.json"
+
+#get stock market api key
+with open('../config/credentials.json') as f:
+    credentials = json.load(f)
+api_key = credentials['stock_market_key']
+api_url = credentials['url']
+
+#load scraped tickers
+def load_tickers_from_file(filename=ticker_file):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def get_data(ticker) -> dict:
+    r = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/2023-01-09/2023-01-09?apiKey={api_key}")
+    if not r.ok:
+        raise Exception(f"Couldn't retrieve API data for ticker {ticker}")
+    data = r.json()
+    print(f"Retrieving data for {ticker}...")
+    return data
+
+
+#flatten nested json returned by api
+def flatten_json(json_obj, parent_key='', sep='_') -> dict:
+    items=[]
+    if isinstance(json_obj, dict):
+        for key, value in json_obj.items():
+            new_key = f"{parent_key}{sep}{key}" if parent_key else key
+            items.extend(flatten_json(value, new_key, sep=sep).items())
+    elif isinstance(json_obj, list):
+        for i, value in enumerate(json_obj):
+            new_key = f"{parent_key}{sep}{i}" if parent_key else str(i)
+            items.extend(flatten_json(value, new_key, sep=sep).items())
+    else:
+        items.append((parent_key, json_obj))
+    return dict(items)
+
+#Dump json to csv files
+def dump_data_to_files(json_data) -> None:
+    flattened_json = flatten_json(json_data)
+    with open(json_file, 'w') as json_f:
+        json.dump(json_data, json_f)
+
+    df = pd.DataFrame(pd.json_normalize(flattened_json))
+    if os.path.exists(csv_file):
+        df.to_csv(csv_file, index=False, mode='a', header=False)
+    else:
+        df.to_csv(csv_file, index=False, mode='w')
+
+    print(f"JSON file saved at {json_file}")
+    print(f"CSV file saved at {csv_file}")
+
+if __name__ == '__main__':
+    tickers = load_tickers_from_file()
+    all_data = {}
+    for ticker in tickers:
+        try:
+            data = get_data(ticker)
+            all_data[ticker] = data
+        except Exception as e:
+            print(e)
+
+    dump_data_to_files(all_data)
---
+++ scripts/scrape_tickers.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from datetime import date, datetime
+current_date = date.today().strftime('%Y-%m-%d')
+current_time = datetime.now().strftime("%H")
+
+url = 'https://www.tradingview.com/markets/stocks-usa/market-movers-active/'
+json_file = f"../data/tickers_{current_date}.json"
+
+def scrape_tickers(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve the page: Status code {response.status_code}")
+        return []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tickers = []
+    for item in soup.find_all('a', class_='apply-common-tooltip tickerNameBox-GrtoTeat tickerName-GrtoTeat'):
+        tickers.append(item.text.strip())
+
+    return tickers
+
+
+def save_tickers_to_file(tickers, filename=json_file):
+    with open(filename, 'w') as f:
+        json.dump(tickers, f, indent=4)
+
+
+if __name__ == '__main__':
+    tickers = scrape_tickers(url)
+    if tickers:
+        save_tickers_to_file(tickers)
+        ticker_count = len(tickers)
+        print(f"Successfully scraped {ticker_count} tickers")
+    else:
+        print("ERROR: No tickers found.")
